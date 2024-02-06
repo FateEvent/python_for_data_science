72ad4615b0a80b6d0e48a982c1778419544be7c4
faventur <faventur@student.42mulhouse.fr>
2024-02-06 15:52:15 +0100

    ex04: print(type()); need to find out how to discovere whether a parameter is NULL

--- python_0/ex03/NULL_not_found.py
+++ python_0/ex03/NULL_not_found.py
@@ -1,20 +1,4 @@
 def NULL_not_found(object = None) -> int:
 
-	if object != None:
-		types = (bool, int, float, complex, str, list, tuple, set, dict)
-
-		i = 0
-		while i < len(types):
-			x = isinstance(object, types[i])
-			if x == True:
-				break
-			i += 1
-
-		if i == len(types) or object == None:
-			type_str = 'none'
-		else:
-			type_str = str(types[i])
-			type_str = type_str[type_str.find('\'') + 1:type_str.rfind('\'')]
-
-		print(f'The object is of type {type_str}')
+	print(type(object))
 	return 42
