701743fbd7d6fca16e3b76fb1d061063d70ec603
faventur <faventur@student.42mulhouse.fr>
2024-02-07 17:04:37 +0100

    ex05 achieved and added try/catch expressions in both ex04 and ex05

--- python_0/ex04/whatis.py
+++ python_0/ex04/whatis.py
@@ -1,13 +1,30 @@
 import sys
 
-if len(sys.argv) == 2:
-    if len(sys.argv[1]) > 15:
-        sys.exit("AssertionError: too many characters")
-    if sys.argv[1][0] == '0' and sys.argv[1][1] != '.':
-        sys.exit("AssertionError: unexisting number starting with zero")
-    if float(sys.argv[1]) % 2 == 0:
-        print("I'm Even.")
+
+def is_integer(n):
+    try:
+        float(n)
+    except ValueError:
+        return False
     else:
-        print("I'm Odd.")
-else:
-    sys.exit("AssertionError: more than one argument is provided")
+        return float(n).is_integer()
+
+
+try:
+    if len(sys.argv) == 2:
+        if len(sys.argv[1]) > 15:
+            raise AssertionError("too many characters")
+        if sys.argv[1][0] == '0' and sys.argv[1][1] != '.':
+            raise AssertionError("unexisting number starting with zero")
+        if not is_integer(sys.argv[1]):
+            raise AssertionError("argument is not an integer")
+        num = float(sys.argv[1]).is_integer()
+        if num % 2 == 0:
+            print("{num} I'm Even.")
+        else:
+            print("I'm Odd.")
+    else:
+        raise AssertionError("more than one argument is provided")
+except Exception as error:
+    print(f'{type(error).__name__}: {error}')
+    exit()
--- python_0/ex05/building.py
+++ python_0/ex05/building.py
@@ -1,13 +1,45 @@
 import sys
 
 
+def ft_punctuation(match: str):
+
+    punctuation = "!\"#$%&'()*+, -./:;<=>?@[\\]^_`{|}~"
+    for c in match:
+        print("c: --> " + c)
+        if c in punctuation:
+            return 1
+    return 0
+
+
 def analyze_entry(line: str):
     if len(line):
-        if not line.isascii():
-            sys.exit("AssertionError: non ASCII characters in the string")
 
-        print(line)
-    return 0
+        if not line.isascii():
+            raise AssertionError("non ASCII characters present in the string")
+
+        uppers = 0
+        lowers = 0
+        punct_marks = 0
+        spaces = 0
+        digits = 0
+        for c in line:
+            if c.isupper():
+                uppers += 1
+            elif c.islower():
+                lowers += 1
+            elif ft_punctuation(c):
+                punct_marks += 1
+            elif c.isspace():
+                spaces += 1
+            elif c.isdigit():
+                digits += 1
+
+        print(f'The text contains {len(line)} characters:')
+        print(f'{uppers} uppercase letters')
+        print(f'{lowers} lowercase letters')
+        print(f'{punct_marks} punctuation marks')
+        print(f'{spaces} spaces')
+        print(f'{digits} digits')
 
 
 def prompt_a_prompt():
@@ -22,13 +54,29 @@ def prompt_a_prompt():
 
 def main():
 
-    if len(sys.argv) > 2:
-        sys.exit("AssertionError: more than one argument is provided")
-    if len(sys.argv) == 1:
-        text = prompt_a_prompt()
-    else:
-        text = sys.argv[1]
-    analyze_entry(text)
+    try:
+        if len(sys.argv) > 2:
+            raise AssertionError("more than one argument is provided")
+        if len(sys.argv) == 1:
+            try:
+                text = prompt_a_prompt()
+            except Exception as error:
+                print(f'{type(error).__name__}: {error}')
+                exit()
+        else:
+            try:
+                text = sys.argv[1]
+            except Exception as error:
+                print(f'{type(error).__name__}: {error}')
+                exit()
+        try:
+            analyze_entry(text)
+        except Exception as error:
+            print(f'{type(error).__name__}: {error}')
+            exit()
+    except Exception as error:
+        print(f'{type(error).__name__}: {error}')
+        exit()
 
     return 0
 
