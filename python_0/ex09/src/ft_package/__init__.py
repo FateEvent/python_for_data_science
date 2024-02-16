5f1a584a8471f6f16114bfca038766f9b6db1bee
faventur <faventur@student.42mulhouse.fr>
2024-02-16 18:17:16 +0100

    package achieved

--- python_0/ex09/src/ft_package/__init__.py
+++ python_0/ex09/src/ft_package/__init__.py
@@ -1,3 +1,6 @@
-from .ft_filter import ft_filter
-from .check_entry_format import check_entry_format
-
+from .ft_f import ft_filter
+from .ft_p import ft_punctuation
+from .c_e_f import check_entry_format
+from .i_i import is_integer
+from .Loading import ft_tqdm
+from .p_a_p import prompt_a_prompt
