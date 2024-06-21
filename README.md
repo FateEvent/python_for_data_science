8141dac5d5ad7b48501e353dc5467f266c7b9f2c
faventur <faventur@student.42mulhouse.fr>
2024-06-21 16:54:31 +0200

    rotate.py achieved

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ For module 2, I use the [Pillow](https://realpython.com/image-processing-with-th
 To return an an array of RGB pixels, in the `ft_load()` function, I use `np.asarray()`, so that I can reconstruct the image from it in the zoom.py program by making use of the [`Image.fromarray()` function](https://stackoverflow.com/questions/62739851/convert-rgb-arrays-to-pil-image).
 
 I then use the method [`.convert("L")`](https://stackoverflow.com/questions/3823752/display-image-as-grayscale) to convert the image to grayscale and show it with the [matplotLib](https://matplotlib.org/stable) method [.imshow()](https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.imshow.html).
+
+To rotate the image there are [various options](https://stackoverflow.com/questions/31401812/matplotlib-rotate-image-file-by-x-degrees) available, among which the `.rotate()` method from the Pillow library.
--- python_1/ex04/rotate.py
+++ python_1/ex04/rotate.py
@@ -1,4 +1,4 @@
-from PIL import Image
+from PIL import Image, ImageOps
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -8,13 +8,13 @@ from load_image import ft_load
 def main():
     try:
         pixel_arr = ft_load("../img/animal.jpeg").astype(np.uint8)
-        print(pixel_arr)
-        img = Image.fromarray(pixel_arr, "RGB")
-        img = img.crop((455, 110, 850, 490))
-        print(f'New shape after slicing: { np.shape(img) }', end='')
-        print(f' or { np.size(img) }')
-        print(np.asarray(img))
+        img = Image.fromarray(pixel_arr, "RGB").crop((455, 110, 850, 490))
         img = img.convert("L")
+        img = ImageOps.mirror(img)
+        print(np.asarray(img))
+        img = img.rotate(90)
+        print(f'New shape after Transpose: { np.shape(img) }')
+        print(np.array(img))
         plt.imshow(img, cmap='gray', vmin=0, vmax=255)
         plt.show()
 
