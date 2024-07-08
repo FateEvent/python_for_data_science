4c4b8c817e745041ed9d13845b1f092f019f195f
faventur <faventur@student.42mulhouse.fr>
2024-07-08 16:06:28 +0200

    added ex03 ft_calculator.py file to ex04 file

--- Python_3_OOP/ex04/ft_calculator.py
+++ Python_3_OOP/ex04/ft_calculator.py
@@ -1,6 +1,36 @@
 class calculator:
-    """The Calculator class allows to perform operations between \
-two vectors"""
+    """The Calculator class allows to perform elementary operations \
+on a vector and operations between two vectors"""
+
+    def __init__(self, slice):
+        """The __init__() function initialises an instance of the \
+calculator class"""
+        self.slice = slice
+
+    def __add__(self, scalar) -> None:
+        """The add() function allows us to overload the + operator"""
+
+        print([el + scalar for el in self.slice])
+
+    def __mul__(self, scalar) -> None:
+        """The mul() function allows us to overload the * operator"""
+
+        print([el * scalar for el in self.slice])
+
+    def __sub__(self, scalar) -> None:
+        """The sub() function allows us to overload the - operator"""
+
+        print([el - scalar for el in self.slice])
+
+    def __truediv__(self, scalar) -> None:
+        """The truediv() function allows us to overload the / operator"""
+
+        try:
+            if scalar == 0:
+                raise ZeroDivisionError("Division by zero is undefined")
+            print([el / scalar for el in self.slice])
+        except Exception as error:
+            print(f'{type(error).__name__}: {error}')
 
     @staticmethod
     def dotproduct(V1: list[float], V2: list[float]) -> None:
