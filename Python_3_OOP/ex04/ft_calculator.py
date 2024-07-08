e8a8e0f863653fc0583152d6b1d699751fd6d55c
faventur <faventur@student.42mulhouse.fr>
2024-07-08 15:55:45 +0200

    added try/except pairs

--- Python_3_OOP/ex04/ft_calculator.py
+++ Python_3_OOP/ex04/ft_calculator.py
@@ -5,19 +5,28 @@ two vectors"""
     @staticmethod
     def dotproduct(V1: list[float], V2: list[float]) -> None:
 
-        sum = 0
-        for e1, e2 in zip(V1, V2):
-            sum += e1 * e2
-        print(f'Dot product is: { sum }')
+        try:
+            sum = 0
+            for e1, e2 in zip(V1, V2):
+                sum += e1 * e2
+            print(f'Dot product is: { sum }')
+        except Exception as error:
+            print(f'{type(error).__name__}: {error}')
 
     @staticmethod
     def add_vec(V1: list[float], V2: list[float]) -> None:
 
-        res = [e1 + e2 for e1, e2 in zip(V1, V2)]
-        print(f'Vector addition is: { res }')
+        try:
+            res = [e1 + e2 for e1, e2 in zip(V1, V2)]
+            print(f'Vector addition is: { res }')
+        except Exception as error:
+            print(f'{type(error).__name__}: {error}')
 
     @staticmethod
     def sub_vec(V1: list[float], V2: list[float]) -> None:
 
-        res = [e1 - e2 for e1, e2 in zip(V1, V2)]
-        print(f'Vector subtraction is: { res }')
+        try:
+            res = [e1 - e2 for e1, e2 in zip(V1, V2)]
+            print(f'Vector subtraction is: { res }')
+        except Exception as error:
+            print(f'{type(error).__name__}: {error}')
--- Python_3_OOP/ex04/tester.py
+++ Python_3_OOP/ex04/tester.py
@@ -5,3 +5,10 @@ b = [2, 4, 3]
 calculator.dotproduct(a, b)
 calculator.add_vec(a, b)
 calculator.sub_vec(a, b)
+
+a = [5.5, 2]
+calculator.add_vec(a, b)
+
+a = ['baobab']
+b = ['ta mere']
+calculator.sub_vec(a, b)
