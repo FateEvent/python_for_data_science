b5e8e11f5fca59629ec726e3d5c515e07d1bea3f
faventur <faventur@student.42mulhouse.fr>
2024-07-08 20:53:18 +0200

    mean and median methods moved to separated functions

--- Python_4_Dod/README.md
+++ Python_4_Dod/README.md
@@ -3,9 +3,24 @@
 To [access each value](https://stackoverflow.com/a/26660785) of the array of key-value pairs `kwargs`, I can loop through the `dict`ionary with the keys as follows:
 
 ```python
-	for k in kwargs:
+    for k in kwargs:
         print(k, kwargs[k])
+```
+
+The star (`*`) is a sort of wrap for variadic arguments that bundles up the arguments that cannot be counted before the script is interpreted.
+That's why you don't need the star anymore to pass the "bundle" as a parameter to another function once it has been "unbundled" in the primary function:
+
+```Python
+def ft_mean(args: any) -> None:
+<SNIP>
+
+def ft_statistics(*args: any, **kwargs: any) -> None:
+<SNIP>
+    if kwargs[k] == "mean":
+                ft_mean(args)
+<SNIP>
 ```
 
 To [calculate the median](https://www.mathsisfun.com/median.html) of a series of numbers, you have to find either the number which is in the middle of the sorted series if the series is odd or the mean of the two numbers located in the middle if the series is even.
 
+To [compute the quartile](https://en.wikipedia.org/wiki/Quartile)
--- Python_4_Dod/ex00/statistics.py
+++ Python_4_Dod/ex00/statistics.py
@@ -1,3 +1,27 @@
+def ft_mean(args: any) -> None:
+    sum = 0.0
+    for entry in args:
+        sum += float(entry)
+
+    print(f'mean: { round(sum / len(args), 2) }')
+
+
+def ft_median(args: any) -> None:
+
+    sort = sorted(args)
+    length = len(sort)
+    mitm: int = 0
+    index: int = int(length / 2)
+    if length % 2 == 0:
+        index = index - 1
+        mitm = sort[index] + sort[index + 1]
+        mitm = round(mitm / 2, 2)
+    else:
+        mitm = round(sort[index], 2)
+
+    print(f'median: { mitm }')
+
+
 def ft_statistics(*args: any, **kwargs: any) -> None:
     """The program takes as parameters a quantity of unknown numbers \
 and a series of commands and make the mean, median, quartile (25% and \
@@ -5,30 +29,16 @@ and a series of commands and make the mean, median, quartile (25% and \
 
     try:
         for k in kwargs:
-            if kwargs[k] == "mean":
-                sum = 0.0
-                for entry in args:
-                    sum += float(entry)
-
-                print(f'mean: { round(sum / len(args), 2) }')
-
-            if kwargs[k] == "median":
-                sort = sorted(args)
-                length = len(sort)
-                mitm: int = 0
-                index: int = int(length / 2)
-                if length % 2 == 0:
-                    index = index - 1
-                    mitm = sort[index] + sort[index + 1]
-                    mitm = round(mitm / 2, 2)
-                else:
-                    mitm = round(sort[index], 2)
-
-                print(f'median: { mitm }')
-
             if kwargs[k] != "mean" and kwargs[k] != "median" \
                 and kwargs[k] != "quartile" and kwargs[k] != "std" \
                     and kwargs[k] != "var":
                 raise AssertionError("ERROR")
+
+            if kwargs[k] == "mean":
+                ft_mean(args)
+
+            if kwargs[k] == "median":
+                ft_median(args)
+
     except Exception as error:
         print(f'{error}')
